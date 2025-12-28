71a650902c9dcb060ca2ce9d6b009c7c38a68fdd
Vishal Kumar Yadav <vishalcr7m10@gmail.com>
2025-12-28 11:53:40 +0530

    Add Cloudinary domain to Next.js image config to fix product images

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,10 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
         hostname: "images.unsplash.com",
       },
       {
